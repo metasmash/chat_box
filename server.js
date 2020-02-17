0565a136877b3105bc5fdb90f7231649fab69693
Ouali <oualiyannick@hotmail.fr>
2020-02-17 17:34:18 +0100

    reset box

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var express = require("express"); var app = express(); var http = require("http").createServer(app);
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
 
 var io = require("socket.io")(http);
 app.use(express.static("./node_modules"));
@@ -7,7 +9,7 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-http.listen(5050, function() {
+http.listen(3000, function() {
   console.log("listening on *:3000");
 });
 
