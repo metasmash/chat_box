7d46c18ef39b98d996fc0b0859f5da433f39b466
Ouali <oualiyannick@hotmail.fr>
2020-02-18 01:09:41 +0100

    last up to date

--- scripts/client.js
+++ scripts/client.js
@@ -1,13 +1,21 @@
 var client = io();
 var sendBtn = document.getElementById("envoyer");
+var send = true;
 
-
+var map = {}; // You could also use an array
+onkeydown = onkeyup = function(e) {
+  e = e || event; // to deal with IE
+  map[e.keyCode] = e.type == "keydown";
+  if (map[16]) {
+    send = false;
+  } else {
+    send = true;
+  }
+};
 
 client.on("connect", e => {
   console.log("Je suis " + client.id);
 });
-
-
 
 sendBtn.addEventListener("click", e => {
   let msg = document.querySelector("#message").value;
@@ -16,11 +24,9 @@ sendBtn.addEventListener("click", e => {
   document.querySelector("#message").value = "";
 });
 
-
-
 document.querySelector("#message").addEventListener("keyup", function(event) {
   // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
+  if (event.keyCode === 13 && send === true) {
     // Cancel the default action, if needed
     event.preventDefault();
     // Trigger the button element with a click
@@ -28,13 +34,11 @@ document.querySelector("#message").addEventListener("keyup", function(event) {
   }
 });
 
-
-
 client.on("reception_message", msg => {
   console.log(msg);
-  document.querySelector("#messages_box").innerHTML += 
-  '<div class="last_message"style="color:white"></div>';
-  let len = document.querySelectorAll('.last_message').length;
-  document.querySelectorAll(".last_message")[len-1].textContent += `
+  document.querySelector("#messages_box").innerHTML +=
+    '<div class="last_message"style="color:white"></div>';
+  let len = document.querySelectorAll(".last_message").length;
+  document.querySelectorAll(".last_message")[len - 1].textContent += `
 ${msg.user} : ${msg.msg}`;
 });
